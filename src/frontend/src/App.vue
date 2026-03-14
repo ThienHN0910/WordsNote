70894eb646ebb87a948528f59f3fbb6880a2493f
ThienHN <hnt.vn.vn@gmail.com>
2026-03-14 13:53:20 +0700

    ud

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import Navbar from '@/components/Navbar.vue'
+import Navbar from './components/Navbar.vue'
 import { RouterView } from 'vue-router'
 
 </script>
